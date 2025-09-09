919b68cb8de7005f89add3e22f7070941ca27734
santiagos03 <dssalazar-2024b@corhuila.edu.co>
2025-09-08 23:44:44 -0500

    send excercise 2

--- 05-week/01-session/taller-listas/src/Ejercicio2.java
+++ 05-week/01-session/taller-listas/src/Ejercicio2.java
@@ -1,1 +1,21 @@
+// Ejercicio2.java
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        // Creamos la lista simple
+        ListaSimple lista = new ListaSimple();
 
+        // Insertamos elementos
+        lista.insertarFin(1);
+        lista.insertarFin(2);
+        lista.insertarFin(4);
+
+        System.out.println("Lista inicial:");
+        lista.mostrar(); // [1, 2, 4]
+
+        // Insertamos en posición 2 (entre 2 y 4)
+        lista.insertarEnPosicion(3, 2);
+
+        System.out.println("Después de insertar 3 en la posición 2:");
+        lista.mostrar(); // [1, 2, 3, 4]
+    }
+}
